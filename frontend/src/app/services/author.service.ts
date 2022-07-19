7d415e3c8c380ae940f992577b286eb162862d38
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2022-07-19 13:58:13 -0300

    Update author.service.ts

--- frontend/src/app/services/author.service.ts
+++ frontend/src/app/services/author.service.ts
@@ -9,12 +9,29 @@ export class AuthorService {
   url = '/api/autor';
   constructor(private http: HttpClient) { }
 
-  getAuthor() {
+  getAuthors() {
     return this.http.get(this.url);
+  }
+
+  getAuthor(id: string) {
+    return this.http.get(this.url + '/' + id)
+  }
+
+  registerAuthor(author: Author) {
+    return this.http.post(this.url, author);
+  }
+
+  deleteAuthor(id: string) {
+    return this.http.delete(this.url + '/' + id);
+  }
+
+  updateAuthor(id: string, author: Author) {
+    return this.http.put(this.url + '/' + id, author);
   }
 }
 
-export interface Autor {
+export interface Author {
   idautor?:'', 
   autor?:'' 
 }
+
