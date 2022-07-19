39bd5ae8a2a7a903f5b630edd63b0cb7a9461113
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2022-07-19 13:57:03 -0300

    Update answer.service.ts

--- frontend/src/app/services/answer.service.ts
+++ frontend/src/app/services/answer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from  '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -7,19 +7,33 @@ import {HttpClient} from  '@angular/common/http'
 export class AnswerService {
 
   url = '/api/resposta'
-  constructor(private http : HttpClient ) { }
+  constructor(private http: HttpClient) { }
 
-  getAnswers(){
-     return this.http.get(this.url) 
+  getAnswers() {
+    return this.http.get(this.url)
   }
 
-  getAnswer(id:string){
-    return this.http.get(this.url+'/'+id)
+  getAnswer(id: string) {
+    return this.http.get(this.url + '/' + id)
+  }
+
+  registerAnswer(answer: Answer) {
+    return this.http.post(this.url, usuario);
+  }
+
+  deleteAnswer(id: string) {
+    return this.http.delete(this.url + '/' + id);
+  }
+
+  updateAnswer(id: string, answer: Answer) {
+    return this.http.put(this.url + '/' + id, answer);
   }
 
 }
 
-export interface Answer{
-  resposta:'',
-  status:''
+export interface Answer {
+  idResposta: '',
+  idPergunta: '',
+  resposta: '',
+  status: ''
 }
